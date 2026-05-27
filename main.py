090b31c6538291d62842de32db7e94c40395dbba
Hannah-2626 <hmendezr@unal.edu.co>
2026-05-27 11:19:37 -0500

    Initialize main.py structure and main function

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
+import requests
+import os
+import json
+import csv
+from datetime import datetime
 
+# SOLICITUDES WEB
+# Persona 2
+
+
+# PROCESAMIENTO DE DATOS
+# Persona 3
+
+
+# EXPORTACIÓN CSV
+# Persona 4
+
+
+# ESTADÍSTICAS
+# Persona 5
+
+
+# REPORTE FINAL
+# Persona 6
+
+
+def main():
+    print("Inicio del programa")
+
+if __name__ == "__main__":
+    main()
